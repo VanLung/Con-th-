ad1aac4541db8e863a37fa5358671754ca0a42fb
dung1308 <90315963+dung1308@users.noreply.github.com>
2021-10-06 15:59:24 +0700

    Add files via upload

---
+++ src/java/phucnt/servlet/ProductDetailServlet.java
@@ -0,0 +1,99 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package phucnt.servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.SQLException;
+import javax.naming.NamingException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import phucnt.tblProducts.ProductDAO;
+import phucnt.tblProducts.ProductDTO;
+
+/**
+ *
+ * @author Nguyen Tien Dung SE150614
+ */
+@WebServlet(name = "ProductDetailServlet", urlPatterns = {"/ProductDetailServlet"})
+public class ProductDetailServlet extends HttpServlet {
+    private final String DETAIL_PAGE = "details.jsp";
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        PrintWriter out = response.getWriter();
+        String url = DETAIL_PAGE;
+        try {
+            int id = Integer.parseInt(request.getParameter("ID"));
+            ProductDAO dao = new ProductDAO();
+            ProductDTO result =  dao.showByID(id);
+            request.setAttribute("LOAD_DETAIL", result);
+        } catch(SQLException e){
+            log("ShowFlowerServlet _ SQL: " + e.getMessage());
+            response.sendError(461);
+        } catch(NamingException e){
+            log("ShowFlowerServlet _ Naming: " + e.getMessage());
+            response.sendError(461);
+        }
+        finally{
+            request.getRequestDispatcher(url).forward(request, response);
+            out.close();
+        }
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
